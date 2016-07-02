d6ca2f58838cb11d29e21e1ef3b3ca512e3e5bcc
GuillaumeHaben <pyxouze@gmail.com>
2016-07-02 17:53:58 +0200

    Initial commit

---
+++ components/SiteLinks/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { RouteHandler, Link } from 'react-router'
+import { prefixLink } from 'gatsby-helpers'
+import { config } from 'config'
+import './style.css'
+import '../../static/fonts/fontawesome/style.css'
+
+class SiteLinks extends React.Component {
+    render() {
+
+        return (
+            <div className='blog-social'>
+                  <a href={ config.siteTwitterUrl } target="_blank"><i className='fa fa-twitter'></i></a>&nbsp;
+                  <a href={ config.siteGithubUrl } target="_blank"><i className='fa fa-github-alt'></i></a>&nbsp;
+                  <a href={ config.siteEmailUrl } target="_blank"><i className='fa fa-envelope-o'></i></a>
+
+            </div>
+            );
+    }
+}
+
+export default SiteLinks
